fcc94ca5a3cd20c53f5dad79de545f321701efe8
gauravbhainsora <gauravv.bhainsora@gmail.com>
2024-02-06 13:25:17 +0530

    reason

--- main.tf
+++ main.tf
@@ -14,6 +14,10 @@ provider "aws" {
   
 # }
 
+# In Terraform, you cannot use both the count and for_each loops together in the same resource block as they have conflicting behaviors and purposes
+# The conflicting behaviors and purposes of the count and for_each loops in Terraform arise from the fact that count creates a fixed number of resource instances based on a numeric index,
+# while for_each creates a dynamic number of resource instances based on a map or set of values.
+
 
 
 # #USING FOR EACH 
