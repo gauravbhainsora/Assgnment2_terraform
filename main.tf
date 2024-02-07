6ef96dbc1f33fcfa7f9080457a353f3a118a3943
gauravbhainsora <gauravv.bhainsora@gmail.com>
2024-02-07 18:05:12 +0530

    commit

---
+++ main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "ap-south-1"  
+}
+
+# #SINGLE BUCKET
+# resource "aws_s3_bucket" "singleBucket"{ 
+#   bucket = "sennaaaaa"
+# }
+
+
+# #USING COUNT
+# variable "bucket_names_count" {
+#   type    = list(string)
+#   default = ["jinx", "irelia", "ggauravg"]  
+# }
+
+# resource "aws_s3_bucket" "Gary" {
+#   count  = length(var.bucket_names_count)
+#   bucket = var.bucket_names_count[count.index]
+  
+# }
+
+# # In Terraform, you cannot use both the count and for_each loops together in the same resource block as they have conflicting behaviors and purposes
+# # The conflicting behaviors and purposes of the count and for_each loops in Terraform arise from the fact that count creates a fixed number of resource instances based on a numeric index,
+# # while for_each creates a dynamic number of resource instances based on a map or set of values.
+
+
+
+# #USING FOR EACH 
+# variable "bucket_names_foreach" {
+#   type    = set(string)
+#   default = ["garen", "jaxerino", "lucianiah"]  
+# }
+
+# resource "aws_s3_bucket" "s3_bucket" {
+#   for_each = var.bucket_names_foreach
+#   bucket = each.key
+# }
